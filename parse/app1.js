5e2888377ee678bc981b94cf90c802d099a58f92
ChristianDess <christianromero20@aragon.unam.mx>
2023-09-12 00:54:15 -0600

    nueva tarea

---
+++ parse/app1.js
@@ -0,0 +1,13 @@
+    let numero1 = prompt("Ingresa un numero") ;
+    let numero2 = prompt("Ingresa un segundo numero") ;
+    console.log(typeof numero1);
+    console.log(typeof numero2);
+
+    //let resultado=numero1+numero2;
+    resultado = parseInt (numero1) + parseInt (numero2);
+    console.log(resultado)
+    resultado = parseInt (numero1) - parseInt (numero2);
+    console.log(resultado)
+    resultado = parseInt (numero1) * parseInt (numero2);
+    console.log(resultado)
+    
